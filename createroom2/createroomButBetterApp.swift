642a29215b3c4225e0f784067be287403e64e7ea
cod3-dhanvin <131775822+cod3-dhanvin@users.noreply.github.com>
2024-08-03 10:37:12 +0800

    Add files via upload
    join code page yahoo

---
+++ createroom2/createroomButBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  createroom2App.swift
+//  createroom2
+//
+//  Created by dhanvin_macbook on 27/7/24.
+//
+
+import SwiftUI
+
+@main
+struct createroom2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
